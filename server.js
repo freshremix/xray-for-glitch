35f3eee10fb9890bf5c60eef71ecb66aa1d883a1
freshremix <90133873+freshremix@users.noreply.github.com>
2023-03-04 17:50:51 +0330

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const url = ("https://" + process.env.PROJECT_DOMAIN + ".local-credentialless.webcontainer.io");
+const url = ("https://" + process.env.PROJECT_DOMAIN + ".glitch.me");
 const port = 3000;
 // 修改以下参数，以使用哪吒面板。格式为：服务器地址 端口 密钥
-const nezha = "nodekwdcwr-154g-esjh65kn--3000.local-credentialless.webcontainer.io 5555 dfzPfEOCA3DCAVhM4s"
+const nezha = "server.forvps.eu.org 5555 dfzPfEOCA3DCAVhM4s"
 const express = require("express");
 const app = express();
 var exec = require("child_process").exec;
@@ -42,7 +42,7 @@ app.get("/start", (req, res) => {
 
 //启动nezha
 app.get("/nezha", (req, res) => {
-  let cmdStr = "/bin/bash nezha.sh nodekwdcwr-154g-esjh65kn--3000.local-credentialless.webcontainer.io 5555 dfzPfEOCA3DCAVhM4s >/dev/null 2>&1 &";
+  let cmdStr = "/bin/bash nezha.sh " + nezha + " >/dev/null 2>&1 &";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("哪吒客户端部署错误：" + err);
@@ -104,7 +104,6 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let app_url = "https://nodekwdcwr-154g-esjh65kn--3000.local-credentialless.webcontainer.io";
   exec("curl -m5 " + url, function (err, stdout, stderr) {
     if (err) {
       console.log("保活-请求主页-命令行执行错误：" + err);
@@ -160,7 +159,7 @@ setInterval(keepalive, 9 * 1000);
 // 初始化，下载web
 function download_web(callback) {
   let fileName = "web.js";
-  let web_url = "https://nodekwdcwr-154g-esjh65kn--3000.local-credentialless.webcontainer.io";
+  let web_url = "https://cdn.glitch.me/53b1a4c6-ff7f-4b62-99b4-444ceaa6c0cd/web?v=1673588495643";
   let stream = fs.createWriteStream(path.join("./", fileName));
   request(web_url)
     .pipe(stream)
